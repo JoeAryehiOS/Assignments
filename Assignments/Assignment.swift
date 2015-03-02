7a4583ac4d3f5c78fe00e54fb1dbf82d2caee879
Aryeh49 <aryeh@oklieb.net>
2015-03-02 15:09:26 -0500

    Added Assignments class

---
+++ Assignments/Assignment.swift
@@ -0,0 +1,9 @@
+//
+//  Assignment.swift
+//  Assignments
+//
+//  Created by Aryeh Lieberman on 3/2/15.
+//  Copyright (c) 2015 JoeAryehiOS. All rights reserved.
+//
+
+import Foundation
--- Assignments/SecondViewController.swift
+++ Assignments/SecondViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import EventKit
+
 
 class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
+  
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
