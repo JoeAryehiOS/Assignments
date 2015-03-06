f26e7fd086fb7ce539a178400ac091d6e2c86c5e
Aryeh49 <aryeh@oklieb.net>
2015-03-06 08:20:00 -0500

    This is weird

---
+++ Assignments/Assignment.swift
@@ -0,0 +1,12 @@
+//
+//  Assignment.swift
+//  Assignments
+//
+//  Created by Aryeh Lieberman on 3/5/15.
+//  Copyright (c) 2015 JoeAryehiOS. All rights reserved.
+//
+
+import Foundation
+class Assignment{
+    
+}
