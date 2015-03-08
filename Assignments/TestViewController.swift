974623ec21b497c67a2a65be6ea17dd597737254
Aryeh49 <aryeh@oklieb.net>
2015-03-07 20:12:22 -0500

    testing

---
+++ Assignments/TestViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TestViewController.swift
+//  Assignments
+//
+//  Created by Aryeh Lieberman on 3/7/15.
+//  Copyright (c) 2015 JoeAryehiOS. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func TestAssignment(sender: AnyObject) {
+        var a: Assignment = Assignment(assignmentName: "10.2", dueDate: NSDate(), timeToComplete: 2.2, details: "#1,2,3")
+            a.setTimeToComplete(10.0, newTimeMinutes: 10.0)
+        println(a.timeToComplete)
+            
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
