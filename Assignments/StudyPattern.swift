551930708d680bda9c955b89304e191902b7575f
Aryeh49 <aryeh@oklieb.net>
2015-05-18 15:10:33 +0200

    Study Pattern
    I’ve added a subclass of UIView called StudyPatternView and a swift
    file containing the Study Pattern class.  The view class will work with
    the view in the work space below the timer graphically displaying the
    study pattern.  The study pattern class will represent a study pattern.
     e.g. 25 minutes work 5 minutes break.

---
+++ Assignments/StudyPattern.swift
@@ -0,0 +1,13 @@
+//
+//  StudyPattern.swift
+//  Assignments
+//
+//  Created by Aryeh Lieberman on 5/18/15.
+//  Copyright (c) 2015 JoeAryehiOS. All rights reserved.
+//
+
+import Foundation
+
+class StudyPattern{
+    
+}
---
+++ Assignments/StudyPatternView.swift
@@ -0,0 +1,21 @@
+//
+//  StudyPatternView.swift
+//  Assignments
+//
+//  Created by Aryeh Lieberman on 5/18/15.
+//  Copyright (c) 2015 JoeAryehiOS. All rights reserved.
+//
+
+import UIKit
+
+class StudyPatternView: UIView {
+    
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+
+
+}
